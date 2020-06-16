c89a46b0810d5f5e0471574a5171842bcc15b844
eskalimur <killer2000@gmx.at>
2020-06-16 10:10:40 +0200

    .

--- inc/gallery.php
+++ inc/gallery.php
@@ -5,6 +5,34 @@
 </head>
 
 <body>
+
+    <div class="container">
+        <header class="page-header">
+            <h1>Auswahl Bilder</h1>
+        </header>
+        <div class="row">
+            <div class="col-md-3">
+                <nav>
+                    <?php
+
+                    ?>
+                </nav>
+            </div>
+            <div class="col-md-9">
+                <main id="content">
+                    <?php
+
+                    include("inc/showpictures.php");
+
+                    ?>
+                </main>
+            </div>
+        </div>
+
+    </div>
+
+
+
 </body>
 
 </html>
