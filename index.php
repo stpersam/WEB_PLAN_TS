fbee8202016ffd68e3c7903af1440ec6474abc1c
eskalimur <killer2000@gmx.at>
2020-06-16 10:01:39 +0200

    .

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>UE11</title>
     <meta charset="utf-8">
@@ -7,19 +8,25 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+    <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <style>
-        body{
+        body {
             margin-left: 30%;
             margin-right: 30%;
         }
     </style>
 </head>
+
 <body>
-<?php
-include "inc/loginFrom.php";
-include "inc/registerForm.php";
-include "inc/gallery.php";
-?>
+    <?php
+    include "inc/loginFrom.php";
+    include "inc/registerForm.php";
+    include "inc/gallery.php";
+    ?>
 
 </body>
+
 </html>
