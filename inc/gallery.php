4a228162ecce16ccc3d095ed4da907e15cb08c01
eskalimur <killer2000@gmx.at>
2020-06-29 13:49:42 +0200

    .

--- inc/gallery.php
+++ inc/gallery.php
@@ -2,131 +2,148 @@
 <html>
 
 <head>
-    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 </head>
 
 <body>
-
-    <div class="">
-        <header class="page-header">
-            <h1>Gallery</h1>
-        </header>
-        <!-- TBD with ajax -> do fileupload -->
-        <div class="row justify-content-between">
-            <h5>Upload new file:</h5>
-            <form action="index.php?use=gallery" method="POST" enctype="multipart/form-data">
-                <input type="file" name="myfile">
-                <input type="submit" name="submitfile">
-            </form>
-        </div>
-
-        <div class="row justify-content-between">
-            <div class="col-md-6">
-                <div class="row">
-                    <div class="dropdown">
-                        <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Show Pictures:
-                        </button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-
-                            <form method="POST" action="index.php?use=gallery">
-                                <input type="text" name="user" value="<?php echo $currentuser; ?>" hidden>
-                                <input class="btn btn-link " type="submit" name="show" value="My pictures">
-                            </form>
-
-                            <form method="POST" action="index.php?use=gallery">
-                                <input type="text" name="userstate" value="<?php echo $currentuser; ?>" hidden>
-                                <input class="btn btn-link" type="submit" name="show" value="My published pictures">
-                            </form>
-
-                            <form method="POST" action="index.php?use=gallery">
-                                <input type="text" name="state" value="freigegeben" hidden>
-                                <input class="btn btn-link" type="submit" name="show" value="All published pictures">
-                            </form>
-
-                        </div>
-                    </div>
-
-                    <!-- TBD with ajax -> use usort() in showpictures with picsort POST value determening which sort -->
-                    <div class="dropdown">
-                        <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Sort by:
-                        </button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <form method="POST" action="index.php?use=gallery">
-                                <input type="text" name="picsort" value="createdate" hidden>
-                                <input class="btn btn-link" type="submit" name="sort" value="Date: created">
-                            </form>
-
-                            <form method="POST" action="index.php?use=gallery">
-                                <input type="text" name="picsort" value="changedate" hidden>
-                                <input class="btn btn-link" type="submit" name="sort" value="Date: changed">
-                            </form>
-                            Default: (date created)
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="d-flex flex-row-reverse  col-md-6">
-                <form method="POST" action="index.php?use=gallery">
-                    <input type="text" name="searchtag" placeholder="Searchterm.."></input>
-                    <input class="btn btn-secondary" type="submit" name="show" value="Search">
-
-                </form>
-            </div>
-        </div>
-
-
-
-        <br></br>
-
-        <div class="col-md-12">
-            <main id="content">
-                <div class="row">
-                    <?php
-                    include("inc/showpictures.php");
-                    ?>
-                </div>
-            </main>
-        </div>
-    </div>
-
-    <br></br>
-
     <div>
         <header class="page-header">
-            <div class="row justify-content-between">
-                <h1>View on Google Maps</h1>
-                <a onclick="CollapseMaps()"><button class="btn-outline-dark">toggle</button></a>
-            </div>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark justify-content-center sticky-top">
+                <a class="navbar-brand" href="">Gallery</a>
+                <div class="">
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown"><a class="nav-link dropdown-toggle" data-toggle="dropdown" href="">Show Pictures</a>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" onclick="show()" href="#">My Pictures</a>
+                                <a class="dropdown-item" href="#">My published Pictures</a>
+                                <a class="dropdown-item" href="#">All published pictures</a>
+                            </div>
+                        </li>
+                        <li class="nav-item"><a class="nav-link" onclick="sort()">Sort</a></li>
+                        <li class="nav-item"><a class="nav-link" onclick="upload()">Upload</a></li>
+                    </ul>
+                </div>
+            </nav>
         </header>
-        <br>
-        <div id="googlemaps" style="block">
-            <div class="cold-md-12" id="map" style="width: 100%; height: 450px">
+
+        <div class="container">
+            <!-- TBD with ajax -> do fileupload -->
+            <div class="row justify-content-between">
+                <h5>Upload new file:</h5>
+                <form action="index.php?use=gallery" method="POST" enctype="multipart/form-data">
+                    <input type="file" name="myfile">
+                    <input type="submit" name="submitfile">
+                </form>
             </div>
+
+            <div class="row justify-content-between">
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="dropdown">
+                            <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Show Pictures:
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+
+                                <form method="POST" action="index.php?use=gallery">
+                                    <input type="text" name="user" value="<?php echo $currentuser; ?>" hidden>
+                                    <input class="btn btn-link " type="submit" name="show" value="My pictures">
+                                </form>
+
+                                <form method="POST" action="index.php?use=gallery">
+                                    <input type="text" name="userstate" value="<?php echo $currentuser; ?>" hidden>
+                                    <input class="btn btn-link" type="submit" name="show" value="My published pictures">
+                                </form>
+
+                                <form method="POST" action="index.php?use=gallery">
+                                    <input type="text" name="state" value="freigegeben" hidden>
+                                    <input class="btn btn-link" type="submit" name="show" value="All published pictures">
+                                </form>
+
+                            </div>
+                        </div>
+
+                        <!-- TBD with ajax -> use usort() in showpictures with picsort POST value determening which sort -->
+                        <div class="dropdown">
+                            <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Sort by:
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <form method="POST" action="index.php?use=gallery">
+                                    <input type="text" name="picsort" value="createdate" hidden>
+                                    <input class="btn btn-link" type="submit" name="sort" value="Date: created">
+                                </form>
+
+                                <form method="POST" action="index.php?use=gallery">
+                                    <input type="text" name="picsort" value="changedate" hidden>
+                                    <input class="btn btn-link" type="submit" name="sort" value="Date: changed">
+                                </form>
+                                Default: (date created)
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex flex-row-reverse  col-md-6">
+                    <form method="POST" action="index.php?use=gallery">
+                        <input type="text" name="searchtag" placeholder="Searchterm.."></input>
+                        <input class="btn btn-secondary" type="submit" name="show" value="Search">
+
+                    </form>
+                </div>
+            </div>
+
+
+
+            <br></br>
+
+            <div class="col-md-12">
+                <main id="content">
+                    <div class="row">
+                        <?php
+                        include("inc/showpictures.php");
+                        ?>
+                    </div>
+                </main>
+            </div>
+
+
+            <br></br>
+
+            <div>
+                <header class="page-header">
+                    <div class="row justify-content-between">
+                        <h1>View on Google Maps</h1>
+                        <a onclick="CollapseMaps()"><button class="btn-outline-dark">toggle</button></a>
+                    </div>
+                </header>
+                <br>
+                <div id="googlemaps" style="display: block">
+                    <div class="cold-md-12" id="map" style="width: 100%; height: 450px"></div>
+                </div>
+            </div>
+
+
+
+            <script type="text/javascript">
+                var myOptions = {
+                    zoom: 8,
+                    center: new google.maps.LatLng(48.20, 16.36),
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                };
+
+                var map = new google.maps.Map(document.getElementById("map"), myOptions);
+
+
+                function CollapseMaps() {
+                    var x = document.getElementById("googlemaps");
+                    if (x.style.display === "none" || x.style.display === "") {
+                        x.style.display = "block";
+                    } else {
+                        x.style.display = "none";
+                    }
+                }
+            </script>
         </div>
     </div>
-    <script type="text/javascript">
-        var myOptions = {
-            zoom: 8,
-            center: new google.maps.LatLng(48.20, 16.36),
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-
-        var map = new google.maps.Map(document.getElementById("map"), myOptions);
-
-
-        function CollapseMaps() {
-            var x = document.getElementById("googlemaps");
-            if (x.style.display === "none") {
-                x.style.display = "block";
-            } else {
-                x.style.display = "none";
-            }
-        }
-    </script>
-
-
 </body>
 
 </html>
--- index_2.php
+++ index_2.php
@@ -15,10 +15,10 @@
     <noscript>
         <link rel="stylesheet" href="res/assets/css/noscript.css" /></noscript>
 
-
+   
     <!-- Fancybox/Fancyapps-->
     <!-- 1. Add latest jQuery and fancybox files -->
-    <!--<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.min.js"></script>-->
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
     <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script>
 
@@ -26,12 +26,10 @@
     <!-- CSS only -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <!-- JS, Popper.js, and jQuery -->
-    <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-   -->
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
-
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
 
 </head>
 
@@ -45,31 +43,33 @@
     } ?>
 
     <script type="text/javascript">
+        $(document).ready(function() {
+            $("#includecontent").load("inc/loginForm.php");
+        });
+
         function showcontent($id) {
             switch ($id) {
                 case "gallery":
                     $("#includecontent").load("inc/gallery.php");
                     break;
+                case "impressum":
+                    $("#includecontent").load("inc/impressum.php");
+                    break;
+                case "help":
+                    $("#includecontent").load("inc/hilfe.php");
+                    break;
                 case "chat":
                     $("#includecontent").load("inc/chat.php");
+                    break;
                 default:
                     break;
-
             }
-        };
-
-        function removegallery() {
-            $("#galleryajax").empty();
         }
     </script>
 
-
-
     <div id="page-wrapper">
-
         <!-- Header -->
         <div id="header">
-
             <!-- Inner -->
             <div class="inner">
                 <header>
@@ -77,9 +77,6 @@
                     <hr />
                     <p>The world greatest Picture Gallery Manager</p>
                 </header>
-                <footer>
-                    <a href="#content" class="button circled scrolly">Start</a>
-                </footer>
             </div>
 
             <!-- Nav -->
@@ -92,14 +89,15 @@
                     <li><a href="" onclick="showcontent('impressum')">Impressum</a></li>
                 </ul>
             </nav>
-
         </div>
 
         <!-- Content -->
         <section id="content">
 
             <body>
-                <div class="container" id="includecontent"></div>
+                <div id="includecontent">
+
+                </div>
 
 
             </body>
@@ -409,15 +407,15 @@
         
     </div>
     -->
-    <!-- Scripts -->
-    <script src="res/assets/js/jquery.min.js"></script>
-    <script src="res/assets/js/jquery.dropotron.min.js"></script>
-    <script src="res/assets/js/jquery.scrolly.min.js"></script>
-    <script src="res/assets/js/jquery.scrollex.min.js"></script>
-    <script src="res/assets/js/browser.min.js"></script>
-    <script src="res/assets/js/breakpoints.min.js"></script>
-    <script src="res/assets/js/util.js"></script>
-    <script src="res/assets/js/main.js"></script>
+        <!-- Scripts -->
+        <script src="res/assets/js/jquery.min.js"></script>
+        <script src="res/assets/js/jquery.dropotron.min.js"></script>
+        <script src="res/assets/js/jquery.scrolly.min.js"></script>
+        <script src="res/assets/js/jquery.scrollex.min.js"></script>
+        <script src="res/assets/js/browser.min.js"></script>
+        <script src="res/assets/js/breakpoints.min.js"></script>
+        <script src="res/assets/js/util.js"></script>
+        <script src="res/assets/js/main.js"></script>
 
 </body>
 
