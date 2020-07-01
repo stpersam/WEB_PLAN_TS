8c60b1bf9e6609bd3a241236d5c9c7be79537a0c
Samuel <samuel.pernthaler@gmail.com>
2020-07-01 15:32:39 +0200

    maps

--- inc/gallery.php
+++ inc/gallery.php
@@ -127,57 +127,23 @@ if (isset($_SESSION['users']['Username'])) {
                     }
                 }
 
-                var contentString = '<div id="content">'+
-                    '<div id="siteNotice">'+
-                    '</div>'+
-                    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-                    '<div id="bodyContent">'+
-                    '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-                    'sandstone rock formation in the southern part of the '+
-                    'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-                    'south west of the nearest large town, Alice Springs; 450&#160;km '+
-                    '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-                    'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-                    'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-                    'Aboriginal people of the area. It has many springs, waterholes, '+
-                    'rock caves and ancient paintings. Uluru is listed as a World '+
-                    'Heritage Site.</p>'+
-                    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-                    'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-                    '(last visited June 22, 2009).</p>'+
-                    '</div>'+
-                    '</div>';
-
-                var infowindow = new google.maps.InfoWindow({
-                    content: contentString
-                });
-
-                // Marker erstellen mit position un Text
+                // Marker erstellen mit position, Text und Bild;
                 var myLatLng = {lat: 48.20, lng: 16.36};
                 var myLatLng2 = {lat: 48.50, lng: 16.50};
-                setMarkers(myLatLng,"Marker",'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png');
-                setMarkers(myLatLng2,"Marker2",'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png');
+                setMarkers(myLatLng,"Marker",'./pictures/thumbnail/test.png');
+                setMarkers(myLatLng2,"Marker2",'./pictures/thumbnail/test.png');
 
                 // set marker
                 function setMarkers(myLatLng,text,url) {
+                    var contentString = '<img src="'+url+'">';
 
-                    var image = {
-                        url: url,
-                        size: new google.maps.Size(20, 32),
-                        origin: new google.maps.Point(0, 0),
-                        anchor: new google.maps.Point(0, 32)
-                    };
-
-                    var shape = {
-                        coords: [1, 1, 1, 20, 18, 20, 18, 1],
-                        type: 'poly'
-                    };
+                    var infowindow = new google.maps.InfoWindow({
+                        content: contentString<
+                    });
 
                     var marker = new google.maps.Marker({
                         position: myLatLng,
                         map: map,
-                        icon: image,
-                        shape: shape,
                         title: text
                     });
 
