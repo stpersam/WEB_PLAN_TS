c85069b13611baad73864a9f8d179a74daf6b036
eskalimur <killer2000@gmx.at>
2020-07-01 16:58:37 +0200

    .

--- inc/pictureupload.php
+++ inc/pictureupload.php
@@ -2,29 +2,44 @@
 <html>
 
 <head>
+    <script type="text/javascript" src="./utility/showcontents.js"></script>
+    <script type="text/javascript" src="./utility/showgallerycontent.js"></script>
+
+
+    <script type="text/javascript">
+        function refreshi() {
+            showcontent("gallery");
+            showgallerycontent("fileupload");
+        }
+    </script>
+
+
 </head>
+
 
 <body>
     <!-- TBD with ajax -> do fileupload -->
     <h5>Upload new file:</h5>
-    <form action="" method="POST" enctype="multipart/form-data">
-        <input type="file" name="myfile" accept="image/*">
+    <form action="inc/pictureupload.php" method="POST" enctype="multipart/form-data">
+        <input type="file" name="myfile" id="myfile" accept="image/*">
         <input type="submit" name="submitfile">
     </form>
 
     <?php
-    if (isset($_POST["myfile"])) {
-        $tmpfile = $_POST["myfile"];
+    if (isset($_FILES['myfile']['type'])) {
+        $tmpfile = $_FILES['myfile']['type'];
 
-        if (($tmpfile !== IMAGETYPE_GIF) && ($tmpfile !== IMAGETYPE_JPEG) && ($tmpfile !== IMAGETYPE_PNG)) {
+        if ($tmpfile != "image/png" && $tmpfile != "image/jpg" && $tmpfile != "image/gif") {
             die("Not a gif/jpeg/png");
         } else {
+
             $tempDateiName = $_FILES["myfile"]["tmp_name"];
             $originalName = $_FILES["myfile"]["name"];
             $eineId = uniqid();
 
-            $dateiPfadNeu = "../pictures/full" . $eineId . $originalName;
+            $dateiPfadNeu = "../pictures/full/" . $eineId . $originalName;
             move_uploaded_file($tempDateiName, $dateiPfadNeu);
+            header("Location: ../index.php");
         }
     }
 
