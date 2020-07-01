5320929792ef95c2c70eed5cfece518ccf9f21cd
eskalimur <killer2000@gmx.at>
2020-07-01 15:33:20 +0200

    .

--- inc/pictureupload.php
+++ inc/pictureupload.php
@@ -5,12 +5,30 @@
 </head>
 
 <body>
-     <!-- TBD with ajax -> do fileupload -->
+    <!-- TBD with ajax -> do fileupload -->
     <h5>Upload new file:</h5>
-    <form action="index.php?use=gallery" method="POST" enctype="multipart/form-data">
-        <input type="file" name="myfile">
+    <form action="" method="POST" enctype="multipart/form-data">
+        <input type="file" name="myfile" accept="image/*">
         <input type="submit" name="submitfile">
     </form>
+
+    <?php
+    if (isset($_POST["myfile"])) {
+        $tmpfile = $_POST["myfile"];
+
+        if (($tmpfile !== IMAGETYPE_GIF) && ($tmpfile !== IMAGETYPE_JPEG) && ($tmpfile !== IMAGETYPE_PNG)) {
+            die("Not a gif/jpeg/png");
+        } else {
+            $tempDateiName = $_FILES["myfile"]["tmp_name"];
+            $originalName = $_FILES["myfile"]["name"];
+            $eineId = uniqid();
+
+            $dateiPfadNeu = "../pictures/full" . $eineId . $originalName;
+            move_uploaded_file($tempDateiName, $dateiPfadNeu);
+        }
+    }
+
+    ?>
 </body>
 
 </html>
