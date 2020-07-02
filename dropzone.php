bb4502588f29464520e656cb6ca945f4153ef741
eskalimur <killer2000@gmx.at>
2020-07-02 13:53:58 +0200

    .

--- dropzone.php
+++ dropzone.php
@@ -2,8 +2,9 @@
 <meta charset="utf-8">
 
 <script src="./utility/dropzone-5.7.0/dist/dropzone.js"></script>
-<link rel="stylesheet" href="res/assets/css/main.css" />
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<link rel="stylesheet" href="res/assets/css/main.css" />
 <link rel="stylesheet" href="./utility/dropzone-5.7.0/dist/dropzone.css" />
 
 <script>
