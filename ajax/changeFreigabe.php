bf195c457a43d1c1a3853178fc048a3cd1aace33
Samuel <samuel.pernthaler@gmail.com>
2020-07-02 15:22:13 +0200

    löschen

--- ajax/changeFreigabe.php
+++ ajax/changeFreigabe.php
@@ -2,25 +2,27 @@
 include "../utility/DB.php";
 session_start();
 
-if (isset($_SESSION['users']['Username'])) {
-    $currentuser = $_SESSION['users']['Username'];
-}
 
 $db = new DB();
 $db->connect("pictures");
 $id = $_POST["id"];
 $status = $db->getPictureState($id);
 $owner = $db->getOwner($id);
-
-if ($currentuser == $owner || $currentuser == "admin") {
-    if ($status == "freigegeben") {
-        $db->changePictureState($id, "gesperrt");
-        echo "gesperrt";
-    } else if ($status == "gesperrt") {
-        $db->changePictureState($id, "freigegeben");
-        echo "freigegeben";
+if (isset($_SESSION['users']['Username'])) {
+    $currentuser = $_SESSION['users']['Username'];
+    if ($currentuser == $owner || $currentuser == "admin") {
+        if ($status == "freigegeben") {
+            $db->changePictureState($id, "gesperrt");
+            echo "gesperrt";
+        } else if ($status == "gesperrt") {
+            $db->changePictureState($id, "freigegeben");
+            echo "freigegeben";
+        }
+    }else{
+        echo $status;
     }
 }else{
     echo $status;
 }
+
 
--- utility/modifyPictures.php
+++ utility/modifyPictures.php
@@ -7,14 +7,16 @@ $db = new DB();
 if($_GET["do"] == "del"){
     if(isset($_GET["name"])) {
         $owner = $db->getOwner($_GET["name"]);
-        if($_SESSION['users']['Username'] == $owner || $_SESSION['users']['Username'] == "admin") {
-            $db->connect("pictures");
-            $href = $db->getHref($_GET["name"]);
-            $db->deletePicture($_GET["name"]);
-            $href2 = "../pictures/thumbnail/".$href;
-            $href = "../pictures/full/".$href;
-            unlink($href);
-            unlink($href2);
+        if(isset($_SESSION['users']['Username'])) {
+            if ($_SESSION['users']['Username'] == $owner || $_SESSION['users']['Username'] == "admin") {
+                $db->connect("pictures");
+                $href = $db->getHref($_GET["name"]);
+                $db->deletePicture($_GET["name"]);
+                $href2 = "../pictures/thumbnail/" . $href;
+                $href = "../pictures/full/" . $href;
+                unlink($href);
+                unlink($href2);
+            }
         }
     }
     header("Location: ../index.php");
