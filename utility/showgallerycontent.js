e5e90d1af13d946759a866ec942f2b3b2c7570e9
eskalimur <killer2000@gmx.at>
2020-07-02 13:49:17 +0200

    ,

--- utility/showgallerycontent.js
+++ utility/showgallerycontent.js
@@ -19,8 +19,3 @@ function showgallerycontent($id, $currentuser, $sort) {
             break;
     }
 }
-/*
-function showarray($array){
-    $("#includegallerycontent").load("inc/showpictures.php");
-}
-*/
