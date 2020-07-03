0a61cba7882f12b0c9ed4e34a8cc6180aa7edd0f
eskalimur <killer2000@gmx.at>
2020-07-03 10:41:10 +0200

    .

--- inc/impressum.php
+++ inc/impressum.php
@@ -20,11 +20,11 @@
 
         <br>
         <p><strong>Impressum</strong></p>
-        <p>Anbieter:<br />Max Mustermann<br />Musterstraße 1<br />80999 München</p>
-        <p>Kontakt:<br />Telefon: 089/1234567-8<br />Telefax: 089/1234567-9<br />E-Mail: mail@mustermann.de<br />Website: www.mustermann.de</p>
+        <p>Anbieter:<br />Max Mustermann<br />Musterstraße 1<br />1220 Wien</p>
+        <p>Kontakt:<br />Telefon: 043/1234567-8<br />Telefax: 043/1234567-9<br />E-Mail: mail@mustermann.at<br />Website: www.mustermann.at</p>
         <p> </p>
         <p>Bei redaktionellen Inhalten:</p>
-        <p>Verantwortlich nach § 55 Abs.2 RStV<br />Moritz Schreiberling<br />Musterstraße 2<br />80999 München</p>
+        <p>Verantwortlich nach § 55 Abs.2 RStV<br />Moritz Schreiberling<br />Musterstraße 2<br />1220 Wien</p>
         <br>
         <h4>Am Projekt beteiligten Personen</h4><br>
         <p><img src="./res/images/Samuel.png"><br><label>Samuel Pernthaler</label></p>
