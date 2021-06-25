586ecb211cf93019460bd799cc81fd49c13ce7a1
Franz <wi19b084@technikum-wien.at>
2021-06-25 09:21:41 +0200

    .

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <html>
 
 <head>
-    <title>Picture Cloud</title>
+    <title>Plan_ts Mobile App</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
     <!-- load CSS -->
